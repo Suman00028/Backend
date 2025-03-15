d1523a8afcd8c34079ee0ca1115d57a675beabca
Suman <sumanghuman289@gmail.com>
2025-03-15 15:19:18 +0530

    wrote the user login and logout logic, and an authentication logic to check if the JWT is correct or not in order to proceed the process further

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { registerUser }  from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser }  from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
+import verifyJWT from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
@@ -15,5 +16,9 @@ userRouter.route("/register").post(upload.fields([
     }
 ]), registerUser);
 
+userRouter.route("/login").post(loginUser);
+
+// Secured routes
+userRouter.route("/logout").post(verifyJWT, logoutUser);
 
 export default userRouter;
